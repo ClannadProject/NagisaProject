75a6272f63a5be67664caef2f464c9437e4d571a
Knoobie <Knoobie@gmx.de>
2015-03-20 13:25:12 +0100

    added String shortname and another Constructor

--- src/main/java/de/knoobie/project/nagisa/gson/model/bo/TransientEvent.java
+++ src/main/java/de/knoobie/project/nagisa/gson/model/bo/TransientEvent.java
@@ -4,6 +4,7 @@ import de.knoobie.project.clannadutils.common.ListUtils;
 import de.knoobie.project.clannadutils.common.StringUtils;
 import de.knoobie.project.nagisa.gson.model.dto.json.common.Names;
 import de.knoobie.project.nagisa.gson.model.dto.json.event.Event;
+import de.knoobie.project.nagisa.gson.model.dto.json.organisation.OrganisationReleaseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
@@ -14,12 +15,22 @@ class TransientEvent {
     private String startdate;
     private String enddate;
     private String link;
+    private String shortname;
     private String name;
     private String description;
     private String vgmdbLink;
 
     private List<TransientEventRelease> releases = new ArrayList<>();
-
+    
+    public TransientEvent(OrganisationReleaseEvent event) {
+        if (event == null) {
+            return;
+        }
+        this.setName(StringUtils.trim(event.getName()));
+        this.setLink(StringUtils.trim(event.getLink()));
+        this.setShortname(StringUtils.trim(event.getShortname()));
+    }
+    
     public TransientEvent(Event event) {
         if (event == null) {
             return;
@@ -30,6 +41,7 @@ class TransientEvent {
         this.setEnddate(StringUtils.trim(event.getEnddate()));
         this.setLink(StringUtils.trim(event.getLink()));
         this.setVgmdbLink(StringUtils.trim(event.getVgmdbLink()));
+        this.setShortname(StringUtils.EMPTY);
 
         if (!ListUtils.isEmpty(event.getReleases())) {
             event.getReleases().stream().forEach((release) -> {
